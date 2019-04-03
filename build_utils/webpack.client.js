62d4cc34aa3e9b3a60a84057669001e3fd52c216
nadusmilli <nadusmilli@Admins-MacBook-Pro-4.local>
2019-04-03 07:31:42 -0700

    webpack configuration changes.

--- build_utils/webpack.client.js
+++ build_utils/webpack.client.js
@@ -1,12 +1,27 @@
 const path = require("path");
 const SRC_DIR = path.resolve("./src");
+const cssPlugin = require("mini-css-extract-plugin");
 
 const config = env => ({
-    entry: path.join(SRC_DIR, "client/index.js"),
+    entry: path.join(SRC_DIR, "/index.js"),
     output: {
         filename: "bundle.js",
-        path: path.join(SRC_DIR, "../public")
-    }
+        path: path.join(__dirname, "../public")
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(css|scss)$/,
+                use: [
+                    cssPlugin.loader,
+                    "style-loader",
+                    "css-loader",
+                    "sass-loader"
+                ]
+            }
+        ]
+    },
+    plugins: [new cssPlugin()]
 });
 
 module.exports = config;
--- build_utils/webpack.server.js
+++ build_utils/webpack.server.js
@@ -1,13 +1,13 @@
 const path = require("path");
-const SRC_DIR = path.resolve("./src");
+const SERVER_DIR = path.resolve("./server");
 const webpack_node_external = require("webpack-node-externals");
 
 const config = env => ({
     target: "node",
-    entry: path.join(SRC_DIR, "index.js"),
+    entry: path.join(SERVER_DIR, "index.js"),
     output: {
         filename: "bundle.js",
-        path: path.join(SRC_DIR, "../build")
+        path: path.join(__dirname, "../build")
     },
     externals: [webpack_node_external()]
 });
