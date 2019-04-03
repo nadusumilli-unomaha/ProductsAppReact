04b3c2228f68f0f0a7ba9be5ddbd5c58fdcf0fb1
nadusmilli <nadusmilli@Admins-MacBook-Pro-4.local>
2019-04-03 08:24:48 -0700

    Adding loading page to the app.

---
+++ src/pages/Loading.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./Loading.css";
+
+/** Function: Loading
+ *  Arguments: None
+ *  Defenition:
+ *      retunrs the jsx for the loading icon.
+ *  Returns: None
+ **/
+const Loading = props => (
+    <div className="sk-fading-circle">
+        <div className="sk-circle1 sk-circle" />
+        <div className="sk-circle2 sk-circle" />
+        <div className="sk-circle3 sk-circle" />
+        <div className="sk-circle4 sk-circle" />
+        <div className="sk-circle5 sk-circle" />
+        <div className="sk-circle6 sk-circle" />
+        <div className="sk-circle7 sk-circle" />
+        <div className="sk-circle8 sk-circle" />
+        <div className="sk-circle9 sk-circle" />
+        <div className="sk-circle10 sk-circle" />
+        <div className="sk-circle11 sk-circle" />
+        <div className="sk-circle12 sk-circle" />
+    </div>
+);
+
+export default Loading;
