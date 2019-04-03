873b6c855742dde71860201ab4ff029a50dc3295
nadusmilli <nadusmilli@Admins-MacBook-Pro-4.local>
2019-04-03 13:56:31 -0700

    Cleaning index.js and adding index.html to server side rendering.

--- src/index.js
+++ src/index.js
@@ -2,16 +2,17 @@ import "@babel/polyfill";
 import React from "react";
 import { hydrate } from "react-dom";
 import { BrowserRouter } from "react-router-dom";
+import { renderRoutes } from "react-router-config";
 import Routes from "./Routes";
 import configureStore from "./store/configureStore";
 import { Provider } from "react-redux";
 
-const store = configureStore();
+const store = configureStore(window.INITIAL_STATE);
 
 hydrate(
     <Provider store={store}>
         <BrowserRouter>
-            <Routes />
+            <>{renderRoutes(Routes)}</>
         </BrowserRouter>
     </Provider>,
     document.getElementById("root")
