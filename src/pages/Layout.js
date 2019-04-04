17bf2a8dcbbdd8c087db49b70e7a857946f7cbb8
nadusmilli <nadusmilli@Admins-MacBook-Pro-4.local>
2019-04-04 15:44:59 -0700

    Adding a Layout component to divide the header and main code.

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,9 +1,13 @@
 import React from "react";
 import { renderRoutes } from "react-router-config";
+import "./Layout.scss";
 
 const Layout = ({ route }) => (
     <>
-        <header>Header</header>
+        <header>
+            <h1>WSI</h1>
+            <hr />
+        </header>
         <main>{renderRoutes(route.routes)}</main>
         <footer />
     </>
