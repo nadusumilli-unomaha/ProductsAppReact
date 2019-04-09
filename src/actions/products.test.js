39fa3c037c91cdd2a2a2eaceaf61d2d8bd94f6c5
nadusmilli <nadusmilli@Admins-MacBook-Pro-4.local>
2019-04-09 15:22:40 -0700

    Product actions test updated.

--- src/actions/products.test.js
+++ src/actions/products.test.js
@@ -1,16 +1,23 @@
 import '@babel/polyfill'
 import * as productActions from './products'
-import moxios from 'moxios'
+import { products } from '../../server/resources/product/products'
+import configureStore from '../store/configureStore'
 
 describe('Product Actions', () => {
+    let store
+
     beforeEach(() => {
-        moxios.install()
+        store = configureStore()
     })
-    afterEach(() => {
-        moxios.uninstall()
-    })
-    test("return an action with type 'GET_PRODUCTS'", () => {
-        const action = productActions.getProducts()
-        expect(true).toEqual(true)
+
+    test("return an action with type 'GET_PRODUCTS'", async () => {
+        await store.dispatch(productActions.getProducts())
+        let expectedState = {
+            products: {
+                data: products.groups,
+                errors: []
+            }
+        }
+        expect(store.getState()).toEqual(expectedState)
     })
 })
